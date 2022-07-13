8bc13c5fe4ba9fe3b28bc7a4cd7051e93bd63bee
fan379660107 <379660107@qq.com>
2022-07-13 19:40:11 +0800

    适配安装vant完成

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+// 引入 flexble
+import 'amfe-flexible/index.min.js'
+
+Vue.config.productionTip = false
+Vue.use(Vant)
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
